616315cad2cd57826613473d733e569b2a3f2799
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-15 17:33:05 +0100

    tests: fix contextloads test

--- src/test/java/wf/garnier/springboottesting/SpringBootTestingApplicationTests.java
+++ src/test/java/wf/garnier/springboottesting/SpringBootTestingApplicationTests.java
@@ -2,8 +2,10 @@ package wf.garnier.springboottesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestSpringBootTestingApplication.class)
 class SpringBootTestingApplicationTests {
 
 	@Test
