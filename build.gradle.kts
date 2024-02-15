16dd4897913b626537d11fec653e1de36e77340d
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-15 16:34:38 +0100

    initial commit, empty spring boot project

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "wf.garnier"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/wf/garnier/springboottesting/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
