5746f5b5b0635fa50cb597d048fc065de25dca1c
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-16 18:57:38 +0100

    test: singleton container

--- src/test/java/wf/garnier/springboottesting/TestSpringBootTestingApplication.java
+++ src/test/java/wf/garnier/springboottesting/TestSpringBootTestingApplication.java
@@ -5,15 +5,23 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestSpringBootTestingApplication {
 
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(
+			DockerImageName.parse("postgres:latest"));
+
+	static {
+		postgreSQLContainer.start();
+	}
+
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		return postgreSQLContainer;
 	}
 
 	public static void main(String[] args) {
