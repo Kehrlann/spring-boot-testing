84ac836c52c9c677cbee0164d818bb21f20315c0
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-18 21:28:02 +0100

    app: add (not-required) jpa configuration

--- src/test/java/wf/garnier/springboottesting/TodoControllerTests.java
+++ src/test/java/wf/garnier/springboottesting/TodoControllerTests.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@WebMvcTest()
+@WebMvcTest
 class TodoControllerTests {
 
 	@Autowired
