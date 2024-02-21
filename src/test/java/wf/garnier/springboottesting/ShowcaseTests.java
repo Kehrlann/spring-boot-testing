4dfa7e55b4438f864c0db79ade6c776a5c983fd5
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-21 15:01:26 +0100

    tests: add showcasing tests

---
+++ src/test/java/wf/garnier/springboottesting/ShowcaseTests.java
@@ -0,0 +1,36 @@
+package wf.garnier.springboottesting;
+
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.awaitility.Awaitility;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+class ShowcaseTests {
+
+	@Test
+	void jsonComparison() throws JSONException {
+		var expected = "{ \"name\": \"Daniel\", \"root\" :  { \"nestedValue\":  42 }}";
+		var actual = "{ \"root\" :  { \"nestedValue\":  42 }, \"name\": \"Daniel\", \"extraProperty\": \"ignored\"}";
+		JSONAssert.assertEquals(expected, actual, JSONCompareMode.LENIENT);
+	}
+
+	@Test
+	void awaitility() {
+		var random = new Random();
+		var counter = new AtomicInteger(0);
+		Awaitility.await().atMost(Duration.ofMillis(200)).pollInterval(Duration.ofMillis(10)).until(() -> {
+			var tryCount = counter.getAndIncrement();
+			System.out.println("Trying ... " + tryCount);
+			return random.nextInt(10) == 9;
+		});
+	}
+
+
+	// TODO: Hamcrest vs AssertJ
+
+}
