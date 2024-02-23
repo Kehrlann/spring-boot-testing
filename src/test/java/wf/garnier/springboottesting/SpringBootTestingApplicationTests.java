490f37f930cac69939bb2283ff5158e0c7d9d832
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-23 18:17:45 +0100

    tests: move html unit tests to @SpringBootTest

--- src/test/java/wf/garnier/springboottesting/SpringBootTestingApplicationTests.java
+++ src/test/java/wf/garnier/springboottesting/SpringBootTestingApplicationTests.java
@@ -1,15 +1,123 @@
 package wf.garnier.springboottesting;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Import(TestSpringBootTestingApplication.class)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SpringBootTestingApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private WebClient webClient;
+
+	private final String todoText = "test-add-todo-" + UUID.randomUUID();
+
+	@BeforeEach
+	void setUp() {
+		webClient.getOptions().setCssEnabled(false);
+		logout();
+	}
+
+	@Test
+	@Order(1)
+	void noUser() throws IOException {
+		HtmlPage htmlPage = webClient.getPage("/");
+		var loginForm = htmlPage.getElementsByTagName("form").getFirst();
+		assertThat(loginForm.getTextContent()).contains("Please sign in");
+	}
+
+	@Test
+	@Order(2)
+	void showsTitle() throws IOException {
+		HtmlPage htmlPage = login("alice", "password");
+		var title = htmlPage.getElementsByTagName("h1").getFirst().getTextContent();
+		assertThat(title).isEqualToIgnoringCase("todo");
+	}
+
+	@Test
+	@Order(3)
+	void noTodos() throws IOException {
+		HtmlPage htmlPage = login("alice", "password");
+		var todos = htmlPage.querySelectorAll(".todo");
+		assertThat(todos.getLength()).isEqualTo(0);
+	}
+
+	@Test
+	@Order(4)
+	void addTodos() throws IOException {
+		HtmlPage htmlPage = login("alice", "password");
+		HtmlInput newTodoField = (HtmlInput) htmlPage.getElementById("new-todo");
+		HtmlButton addTodoButton = (HtmlButton) htmlPage.getElementById("add-button");
+
+		newTodoField.type(todoText);
+		htmlPage = addTodoButton.click();
+
+		var todos = htmlPage.querySelectorAll(".todo > [data-role=\"content\"]");
+		assertThat(todos).hasSize(1).last().extracting(DomNode::getTextContent).isEqualTo(todoText);
+	}
+
+	@Test
+	@Order(5)
+	void otherUser() throws IOException {
+		HtmlPage htmlPage = login("bob", "password");
+		var todos = htmlPage.querySelectorAll(".todo");
+		assertThat(todos).hasSize(0);
+	}
+
+	/**
+	 * This test depends on {@link #addTodos()}. A test could also be independent by
+	 * adding a todoItem and then deleting it.
+	 * @throws IOException -
+	 */
+	@Test
+	@Order(6)
+	void deleteTodo() throws IOException {
+		HtmlPage htmlPage = login("alice", "password");
+
+		HtmlButton completeTodoButton = htmlPage.querySelector(".todo button");
+		htmlPage = completeTodoButton.click();
+
+		var todos = htmlPage.querySelectorAll(".todo");
+		assertThat(todos).hasSize(0);
+	}
+
+	private HtmlPage login(String username, String password) throws IOException {
+		HtmlPage loginPage = webClient.getPage("/");
+		HtmlInput usernameField = (HtmlInput) loginPage.getElementById("username");
+		HtmlInput passwordField = (HtmlInput) loginPage.getElementById("password");
+		HtmlButton submitButton = (HtmlButton) loginPage.getElementsByTagName("button").getFirst();
+
+		usernameField.type(username);
+		passwordField.type(password);
+
+		return submitButton.click();
+	}
+
+	private void logout() {
+		webClient.getCookieManager().clearCookies();
 	}
 
 }
--- src/test/java/wf/garnier/springboottesting/TodoControllerTests.java
+++ src/test/java/wf/garnier/springboottesting/TodoControllerTests.java
@@ -1,15 +1,6 @@
 package wf.garnier.springboottesting;
 
-import java.io.IOException;
-import java.util.List;
-
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +13,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -82,87 +71,6 @@ class TodoControllerTests {
 				.andExpect(status().is4xxClientError());
 
 			verifyNoInteractions(todoService);
-		}
-
-	}
-
-	@Nested
-	class HtmlUnitTests {
-
-		@Autowired
-		private WebClient webClient;
-
-		@BeforeEach
-		void setUp() {
-			webClient.getCookieManager().clearCookies();
-		}
-
-		@Test
-		void noUser() throws IOException {
-			HtmlPage htmlPage = webClient.getPage("/");
-			var loginForm = htmlPage.getElementsByTagName("form").getFirst();
-			assertThat(loginForm.getTextContent()).contains("Please sign in");
-		}
-
-		@Test
-		@WithMockUser
-		void title() throws IOException {
-			HtmlPage htmlPage = webClient.getPage("/");
-			var title = htmlPage.getElementsByTagName("h1").getFirst().getTextContent();
-			assertThat(title).isEqualToIgnoringCase("todo");
-		}
-
-		@Test
-		@WithMockUser
-		void noTodos() throws IOException {
-			HtmlPage htmlPage = webClient.getPage("/");
-			var todos = htmlPage.querySelectorAll(".todo");
-			assertThat(todos.getLength()).isEqualTo(0);
-		}
-
-		@Test
-		@WithMockUser("test-user")
-		void hasTodos() throws IOException {
-			var username = "test-user";
-			when(todoService.getTodos(username)).thenReturn(List.of(new TodoItem(1L, "Do groceries", username),
-					new TodoItem(2L, "Cook dinner", username), new TodoItem(3L, "Enjoy meal", username)));
-
-			HtmlPage htmlPage = webClient.getPage("/");
-			var todos = htmlPage.querySelectorAll(".todo > [data-role=\"content\"]");
-			assertThat(todos).hasSize(3)
-				.extracting(DomNode::getTextContent)
-				.map(String::trim)
-				.containsExactly("Do groceries", "Cook dinner", "Enjoy meal");
-		}
-
-		@Test
-		@WithMockUser("test-user")
-		void deleteTodo() throws IOException {
-			var username = "test-user";
-			when(todoService.getTodos(username)).thenReturn(
-					List.of(new TodoItem(42L, "Do groceries", username), new TodoItem(25L, "Buy the thing", username)));
-
-			HtmlPage page = webClient.getPage("/");
-			HtmlButton deleteButton = page.querySelectorAll(".todo")
-				.stream()
-				.filter(node -> node.getTextContent().contains("Buy the thing"))
-				.toList()
-				.get(0)
-				.querySelector("button");
-			deleteButton.click();
-
-			verify(todoService).delete(25, username);
-		}
-
-		@Test
-		@WithMockUser("test-user")
-		void addTodo() throws IOException {
-			HtmlPage page = webClient.getPage("/");
-			page.<HtmlTextInput>querySelector("input[name=\"text\"]").type("Try adding things");
-
-			page.getElementById("add-button").click();
-
-			verify(todoService).addTodo("Try adding things", "test-user");
 		}
 
 	}
