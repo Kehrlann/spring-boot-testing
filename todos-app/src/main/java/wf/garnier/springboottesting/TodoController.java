1dc3917cf09623d7df2049f203d7e7747f433f8f
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-25 17:10:45 +0100

    app: format todo controller

--- todos-app/src/main/java/wf/garnier/springboottesting/TodoController.java
+++ todos-app/src/main/java/wf/garnier/springboottesting/TodoController.java
@@ -10,28 +10,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 class TodoController {
 
-    private final TodoService todoService;
+	private final TodoService todoService;
 
-    TodoController(TodoService todoService) {
-        this.todoService = todoService;
-    }
+	TodoController(TodoService todoService) {
+		this.todoService = todoService;
+	}
 
-    @GetMapping("/")
-    public String index(Model model) {
-        var todos = todoService.getTodos();
-        model.addAttribute("todos", todos);
-        return "/index";
-    }
+	@GetMapping("/")
+	public String index(Model model) {
+		var todos = todoService.getTodos();
+		model.addAttribute("todos", todos);
+		return "/index";
+	}
 
-    @PostMapping("/todo/{id}/delete")
-    public String delete(@PathVariable("id") Long id) {
-        todoService.delete(id);
-        return "redirect:/";
-    }
+	@PostMapping("/todo/{id}/delete")
+	public String delete(@PathVariable("id") Long id) {
+		todoService.delete(id);
+		return "redirect:/";
+	}
 
-    @PostMapping("/todo")
-    public String delete(@RequestParam("text") String text) {
-        todoService.addTodo(text);
-        return "redirect:/";
-    }
+	@PostMapping("/todo")
+	public String delete(@RequestParam("text") String text) {
+		todoService.addTodo(text);
+		return "redirect:/";
+	}
+
 }
