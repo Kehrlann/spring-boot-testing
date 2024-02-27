3e64616c4d0eaf5763b81b5e959c9ecfc1f4da7b
Daniel Garnier-Moiroux <git@garnier.wf>
2024-02-27 10:08:49 +0100

    tests: assertj and BasicJSONTester

--- todos-with-users/src/test/java/wf/garnier/springboottesting/todos/users/ShowcaseTests.java
+++ todos-with-users/src/test/java/wf/garnier/springboottesting/todos/users/ShowcaseTests.java
@@ -1,6 +1,7 @@
 package wf.garnier.springboottesting.todos.users;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,6 +10,9 @@ import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.boot.test.json.BasicJsonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ShowcaseTests {
 
@@ -24,17 +28,44 @@ class ShowcaseTests {
 		var random = new Random();
 		var counter = new AtomicInteger(0);
 		Awaitility.await()
-				.atMost(Duration.ofMillis(500))
-				.pollDelay(Duration.ZERO)
-				.pollInterval(Duration.ofMillis(10))
-				.until(() -> {
-			var tryCount = counter.getAndIncrement();
-			System.out.println("Trying ... " + tryCount);
-			return random.nextInt(10) == 9;
-		});
+			.atMost(Duration.ofMillis(500))
+			.pollDelay(Duration.ZERO)
+			.pollInterval(Duration.ofMillis(10))
+			.until(() -> {
+				var tryCount = counter.getAndIncrement();
+				System.out.println("Trying ... " + tryCount);
+				return random.nextInt(10) == 9;
+			});
 	}
 
+	private BasicJsonTester json = new BasicJsonTester(getClass());
 
-	// TODO: Hamcrest vs AssertJ
+	@Test
+	void assertJ() {
+		//@formatter:off
+		var characters = List.of(
+				new Character("Frodo", "Hobbit"),
+				new Character("Sam", "Hobbit"),
+				new Character("Gandalf", "Wizard"),
+				new Character("Legolas", "Elf")
+		);
+		//@formatter:on
+
+		assertThat(characters).hasSize(3)
+			.filteredOn(c -> c.charClass.equals("Hobbit"))
+			.hasSize(2)
+			.extracting(Character::name)
+			.containsExactlyInAnyOrder("Sam", "Frodo");
+	}
+
+	@Test
+	void assertJWithJson() {
+		var charJson = "{\"characters\": [{\"name\": \"Frodo\", \"charClass\": \"Hobbit\"}, {\"name\": \"Gandalf\", \"charClass\": \"Wizard\"}, {\"name\": \"Sam\", \"charClass\":  \"Hobbit\"}]}";
+		assertThat(json.from(charJson)).extractingJsonPathArrayValue("$.characters[?(@.charClass == 'Hobbit')].name")
+			.containsExactly("Frodo", "Sam");
+	}
+
+	record Character(String name, String charClass) {
+	}
 
 }
