6fb36dde338fa9eb21714c7587e52f8ad8ce3cda
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-03 14:46:44 +0200

    tests: simplify imports

--- todos-app/src/test/java/wf/garnier/springboottesting/todos/simple/TodoApplicationTests.java
+++ todos-app/src/test/java/wf/garnier/springboottesting/todos/simple/TodoApplicationTests.java
@@ -3,20 +3,20 @@ package wf.garnier.springboottesting.todos.simple;
 import java.io.IOException;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -54,6 +54,20 @@ class TodoApplicationTests {
 
 		var addedToto = page.querySelector(".todo > [data-role=\"content\"]").getTextContent();
 		assertThat(addedToto).isEqualTo("this is a todo");
+	}
+
+	@Test
+	void itDoesJavascript() throws IOException {
+		HtmlPage page = webClient.getPage("/");
+
+		HtmlInput input = page.querySelector("form > input");
+		HtmlButton button = (HtmlButton) page.getElementById("add-button");
+
+		input.type("this is an INVALID todo");
+		page = button.click();
+
+		var allTodos = page.querySelectorAll(".todo > [data-role=\"content\"]").stream().map(DomNode::getTextContent);
+		assertThat(allTodos).doesNotContain("this is an INVALID todo");
 	}
 
 }
