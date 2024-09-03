fed732097906acbefa197f4864de6392aea36ba8
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-03 14:44:53 +0200

    app: fix devtools dependency

--- todos-app/build.gradle.kts
+++ todos-app/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
-    developmentOnly("org.springframework.boot:spring-boot-dev-tools")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
