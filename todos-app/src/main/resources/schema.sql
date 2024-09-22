4600e0afb18cabc8df1da4333d1e708b496042a4
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-22 17:27:08 +0200

    demo: remove postgres

--- todos-app/src/main/resources/schema.sql
+++ todos-app/src/main/resources/schema.sql
@@ -2,11 +2,5 @@ CREATE TABLE IF NOT EXISTS todo_item
 (
     id          SERIAL PRIMARY KEY NOT NULL,
     text        TEXT               NOT NULL,
-    description TEXT DEFAULT NULL,
-    ts          tsvector GENERATED ALWAYS AS (
-        setweight(to_tsvector('english', text), 'A') ||
-        setweight(to_tsvector('english', description), 'B')
-        ) STORED
+    description TEXT DEFAULT NULL
 );
-
-CREATE INDEX IF NOT EXISTS todo_ts_idx ON todo_item USING GIN (ts);
