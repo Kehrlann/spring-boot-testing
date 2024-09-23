3ea4b497bf1d6f3acd6a08198f63ae0e484cde0a
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-23 10:24:37 +0200

    demo: remove slow bean

--- todos-app/src/main/java/wf/garnier/springboottesting/todos/simple/TodoConfiguration.java
+++ todos-app/src/main/java/wf/garnier/springboottesting/todos/simple/TodoConfiguration.java
@@ -1,10 +1,5 @@
 package wf.garnier.springboottesting.todos.simple;
 
-import java.time.Duration;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -12,23 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class TodoConfiguration {
-
-	Logger logger = LoggerFactory.getLogger(TodoConfiguration.class);
-
-	public record SlowStartingBean() {
-	}
-
-	// @Bean
-	public SlowStartingBean slowStartingBean() throws InterruptedException {
-		logger.info("🐌 ... configuring ...");
-		Thread.sleep(Duration.ofSeconds(1));
-		logger.info("🐌 ... slow ...");
-		Thread.sleep(Duration.ofSeconds(1));
-		logger.info("🐌 ... bean ...");
-		Thread.sleep(Duration.ofSeconds(1));
-		logger.info("🐌🏆 ... slow ... bean ... successfully... configured ...");
-		return new SlowStartingBean();
-	}
 
 	@Bean
 	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> connectorCustomizer() {
