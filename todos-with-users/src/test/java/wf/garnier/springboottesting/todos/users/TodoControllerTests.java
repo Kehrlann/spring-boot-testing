ce994b9fef452bf21061b1d20570b97ee9359723
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-29 15:48:29 +0200

    todos-with-users: reintroduce testcontainers

--- todos-with-users/src/test/java/wf/garnier/springboottesting/todos/users/TodoControllerTests.java
+++ todos-with-users/src/test/java/wf/garnier/springboottesting/todos/users/TodoControllerTests.java
@@ -24,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 @WebMvcTest
+@Import(SecurityConfiguration.class)
 class TodoControllerTests {
 
 	@MockitoBean
