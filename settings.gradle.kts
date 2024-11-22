52e5a9a74fba143c5cf6bc479a492e035178fa64
Daniel Garnier-Moiroux <git@garnier.wf>
2024-11-22 12:20:02 +0100

    bump to Boot 3.4.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("org.springframework.boot") version "3.4.0-RC1"
+        id("org.springframework.boot") version "3.4.0"
         id("io.spring.dependency-management") version "1.1.6"
         id("io.spring.javaformat") version "0.0.43"
     }
