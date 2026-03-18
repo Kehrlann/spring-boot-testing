850cbbbee3871e7ed6a794cf7d53efa0849fe0bf
Daniel Garnier-Moiroux <git@garnier.wf>
2026-03-18 15:15:58 +0100

    demo: remove testcontainers

--- todos-app/src/test/java/wf/garnier/springboottesting/todos/simple/TodoApplicationTests.java
+++ todos-app/src/test/java/wf/garnier/springboottesting/todos/simple/TodoApplicationTests.java
@@ -3,14 +3,12 @@ package wf.garnier.springboottesting.todos.simple;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Import(ContainersConfiguration.class)
 class TodoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
