91f5d04c172fc9eee37531f9256023600323bbe0
Daniel Garnier-Moiroux <git@garnier.wf>
2026-03-18 19:44:29 +0100

    tests: fix imports

--- todos-with-users/src/test/java/wf/garnier/springboottesting/todos/users/TodoControllerTests.java
+++ todos-with-users/src/test/java/wf/garnier/springboottesting/todos/users/TodoControllerTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
